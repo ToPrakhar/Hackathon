378d52ec182a3cca0d30cbfbdd062ab5d672f976
ToPrakhar <toprakhargupta@gmail.com>
2015-03-21 17:18:47 -0400

    logout

--- CarPool/WebContent/js/home.js
+++ CarPool/WebContent/js/home.js
@@ -19,6 +19,19 @@ $(".moredetails").on("click", function(){
 	$(this).parent().siblings(".moredetail-child").slideToggle("fast");	
 });
 
+$(".logout").on("click", function(){
+	 	var cookies = document.cookie.split(";");
+
+	    for (var i = 0; i < cookies.length; i++) {
+	    	var cookie = cookies[i];
+	    	var eqPos = cookie.indexOf("=");
+	    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+	    	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+	    }
+	    
+	    location.href = "register.html";
+});
+
 });
 
 
