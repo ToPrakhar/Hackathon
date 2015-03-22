1d169bfefb7f63ff0bbd53c4eaa2b07840924e62
ToPrakhar <toprakhargupta@gmail.com>
2015-03-22 00:43:38 -0400

    home.js prakhar

--- CarPool/WebContent/js/home.js
+++ CarPool/WebContent/js/home.js
@@ -163,13 +163,12 @@ $("#old-user-tap").on("click", function(){
 });
 
 $("#paid-tap").on("click", function(){
+	$("#loadingdemo").show();
+	setTimeout(function(){$("#loadingdemo").hide(); }, 1000);
 });
 
 $("#unpaid-tap").on("click", function(){
-	setTimeout(function(){$("#loadingdemo").show(); }, 1000);
-});
-
-$("#unpaid-tap").on("click", function(){
+	$("#loadingdemo").show();
 	setTimeout(function(){$("#loadingdemo").show(); }, 1000);
 });
 
